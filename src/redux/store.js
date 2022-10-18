f55829b94bf2542f1b5a7dbbf3ad8ccc27845ac0
Yaroslav Semenov <yaryk2013@gmail.com>
2022-10-18 22:33:48 +0300

    deleted comment

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contacts/contactsReducer';
 
-// export const addContact = createAction('contacts/addContact');
-// export const deleteContact = createAction('contacts/deleteContact');
-// export const filterContacts = createAction('contacts/filterContacts');
-
-// const itemsReducer = createReducer([], {
-//   [addContact]: (state, { payload }) => [...state, payload],
-//   [deleteContact]: (state, { payload }) =>
-//     state.filter(contact => contact.id !== payload),
-// });
-
-// const filterReducer = createReducer('', {
-//   [filterContacts]: (_, { payload }) => payload,
-// });
-
-// const contactssReducer = combineReducers({
-//   items: itemsReducer,
-//   filter: filterReducer,
-// });
-
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
